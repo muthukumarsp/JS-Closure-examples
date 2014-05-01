35c80c657a963e26c2aab9296c7922ce0081b983
Muthukumar S P <muthukumarsp@gmail.com>
2014-05-01 10:33:58 +1000

    closure example initial check in

---
+++ README.md
@@ -0,0 +1,8 @@
+JavaScript Closure Example
+--------------------------
+
+What is Closure in JavaScript?
+
+In javascript, inner functions have access to global variables, outer functions variables and parameters even after the outer function returns.
+This is very powerfull and expressive feature in javascipt which is extensively used in jQuery, node.js and many other libraries.
+
---
+++ fade-closureEx.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	function fade(id) {
+		var dom = document.getElementById(id);
+		level = 1;
+		function step() {
+			var h = level.toString(16);
+			dom.style.backgroundColor = "#FFFF" + h + h;
+			if (level < 15) {
+				level += 1;
+				setTimeout(step, 100);
+			}
+		}
+		setTimeout(step, 100);
+	}
+
+	function fadeDocument() {
+		// alert("onload");
+		fade("fade-text");
+		fade("fade-h1");
+	}
+	fadeDocument();
+});
